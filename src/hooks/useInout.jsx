300d3d2d440a27f531026fe4bd1515951b782214
Nadia <nadiahosisy@gmail.com>
2023-09-18 19:00:47 +0300

    add

--- src/hooks/useInout.jsx
+++ src/hooks/useInout.jsx
@@ -1,3 +1,28 @@
-const useInout = (massage) => {};
+import { useState } from "react";
+
+const useInout = (massage, validationFn = null) => {
+  const { value, setValue } = useState("");
+  const { error, setError } = useState({
+    isError: false,
+    massage: "",
+  });
+  const handleChange = (e) => {
+    resetError();
+    setValue(e.target.value);
+  };
+  const handleBlur = () => {
+    let isError = false;
+
+    if (validationFn) {
+      isError = validationFn(value);
+    }
+  };
+  const resetError = () => {
+    setError({
+      isError: false,
+      massage: "",
+    });
+  };
+};
 
 export default useInout;
